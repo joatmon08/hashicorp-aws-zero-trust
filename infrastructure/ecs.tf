d099870db95d875159e8f9048e7f7e462df41f77
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-10-29 12:11:46 -0400

    Add security groups to allow HCP Consul

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -33,6 +33,47 @@ resource "aws_security_group_rule" "boundary_ssh" {
   description              = "Allow SSH from Boundary workers to ECS container instances"
 }
 
+locals {
+  ingress_consul_rules = [
+    {
+      description = "HCP Consul Server RPC"
+      port        = 8300
+      protocol    = "tcp"
+    },
+    {
+      description = "Consul LAN Serf (tcp)"
+      port        = 8301
+      protocol    = "tcp"
+    },
+    {
+      description = "Consul LAN Serf (udp)"
+      port        = 8301
+      protocol    = "udp"
+    },
+    {
+      description = "Consul HTTP"
+      port        = 80
+      protocol    = "udp"
+    },
+    {
+      description = "Consul HTTPS"
+      port        = 443
+      protocol    = "udp"
+    }
+  ]
+}
+
+resource "aws_security_group_rule" "hcp_consul" {
+  count             = length(local.ingress_consul_rules)
+  description       = local.ingress_consul_rules[count.index].description
+  protocol          = local.ingress_consul_rules[count.index].protocol
+  security_group_id = aws_security_group.ecs.id
+  cidr_blocks       = [local.hvn_cidr_block]
+  from_port         = local.ingress_consul_rules[count.index].port
+  to_port           = local.ingress_consul_rules[count.index].port
+  type              = "ingress"
+}
+
 resource "aws_kms_key" "ecs" {
   description             = "${var.name}-ecs"
   deletion_window_in_days = 7
