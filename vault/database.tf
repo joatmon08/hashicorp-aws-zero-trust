7ec13618d8010e21bf973ab6eebcd6a43e8654aa
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-11-02 09:38:38 -0400

    Change to periodic token

--- vault/database.tf
+++ vault/database.tf
@@ -15,7 +15,6 @@ resource "vault_database_secret_backend_connection" "postgres" {
 
 locals {
   products_creds_path = "${vault_mount.postgres.path}/creds/product"
-  boundary_creds_path = "${vault_mount.postgres.path}/creds/boundary"
 }
 
 resource "vault_database_secret_backend_role" "product" {
@@ -39,47 +38,4 @@ data "vault_policy_document" "product" {
 resource "vault_policy" "product" {
   name   = "product"
   policy = data.vault_policy_document.product.hcl
-}
-
-resource "vault_database_secret_backend_role" "boundary" {
-  backend               = vault_mount.postgres.path
-  name                  = "boundary"
-  db_name               = vault_database_secret_backend_connection.postgres.name
-  creation_statements   = ["CREATE ROLE \"{{username}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{username}}\"; GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO \"{{username}}\";"]
-  revocation_statements = ["ALTER ROLE \"{{username}}\" NOLOGIN;"]
-  default_ttl           = 3600
-  max_ttl               = 3600
-}
-
-data "vault_policy_document" "boundary" {
-  rule {
-    path         = local.boundary_creds_path
-    capabilities = ["read"]
-    description  = "read all credentials for product database as boundary"
-  }
-
-  rule {
-    path         = "auth/token/lookup-self"
-    capabilities = ["read"]
-    description  = "lookup token"
-  }
-}
-
-resource "vault_policy" "boundary" {
-  name   = "boundary"
-  policy = data.vault_policy_document.boundary.hcl
-}
-
-resource "vault_token" "boundary" {
-  role_name = vault_token_auth_backend_role.boundary.role_name
-  policies  = [vault_policy.boundary.name]
-  ttl       = "24h"
-}
-
-resource "vault_token_auth_backend_role" "boundary" {
-  role_name           = "boundary"
-  allowed_policies    = [vault_policy.boundary.name]
-  disallowed_policies = ["default"]
-  orphan              = true
-  renewable           = true
 }
