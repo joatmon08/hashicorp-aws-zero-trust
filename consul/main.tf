3be197b6bc867c2fd89776e48f3ee7a14d12c163
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-11-03 10:49:18 -0400

    Remove HCP dependency

--- consul/main.tf
+++ consul/main.tf
@@ -4,14 +4,8 @@ terraform {
       source  = "hashicorp/consul"
       version = "~> 2.14"
     }
-    hcp = {
-      source  = "hashicorp/hcp"
-      version = "~> 0.18"
-    }
   }
 }
-
-provider "hcp" {}
 
 provider "consul" {
   address    = local.consul_addr
--- consul/variables.tf
+++ consul/variables.tf
@@ -9,19 +9,11 @@ data "terraform_remote_state" "hcp" {
   }
 }
 
-data "hcp_consul_cluster" "cluster" {
-  cluster_id = local.consul_cluster_id
-}
-
-resource "hcp_consul_cluster_root_token" "cluster" {
-  cluster_id = local.consul_cluster_id
-}
-
 locals {
   consul_cluster_id = data.terraform_remote_state.hcp.outputs.hcp_consul_id
   consul_addr       = data.terraform_remote_state.hcp.outputs.hcp_consul_public_endpoint
-  consul_datacenter = data.hcp_consul_cluster.cluster.datacenter
-  consul_token      = hcp_consul_cluster_root_token.cluster.secret_id
+  consul_datacenter = data.terraform_remote_state.hcp.outputs.hcp_consul_datacenter
+  consul_token      = data.terraform_remote_state.hcp.outputs.hcp_consul_root_token
 }
 
 
