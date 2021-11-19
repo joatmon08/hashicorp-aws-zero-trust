e780e3f1f44f1357c3e64db9b15cbc68182f2ae8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-11-19 14:20:24 -0500

    Enable SSH to controller

--- infrastructure/boundary.tf
+++ infrastructure/boundary.tf
@@ -1,7 +1,7 @@
 module "boundary" {
   depends_on                   = [module.vpc]
   source                       = "joatmon08/boundary/aws"
-  version                      = "0.1.0"
+  version                      = "0.1.1"
   vpc_id                       = module.vpc.vpc_id
   vpc_cidr_block               = module.vpc.vpc_cidr_block
   public_subnet_ids            = module.vpc.public_subnets
@@ -11,4 +11,5 @@ module "boundary" {
   allow_cidr_blocks_to_workers = [var.client_cidr_block]
   allow_cidr_blocks_to_api     = ["0.0.0.0/0"] # Allow TFC to configure
   boundary_db_password         = var.boundary_database_password
+  enable_ssh_to_controller     = true
 }
